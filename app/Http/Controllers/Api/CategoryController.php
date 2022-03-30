b00271d78a855e3ed1deda9df2dae59b8b7cf739
eboc-2020 <70177453+eboc-2020@users.noreply.github.com>
2022-03-29 22:56:29 -0600

    Creacion de controllador para mostrar info en endpoint y creacion de endpoints(rutas)

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories=Category::all();
+
+        return $categories;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' =>'required|max:255',
+            'slug' =>'required|max:255|unique:categories'
+        ]);
+        $category = Category::create($request->all());
+
+        return $category;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        // $category = Category::findOrFail($id);  es otra forma de obtener el objeto
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' =>'required|max:255',
+            'slug' =>'required|max:255'
+        ]);
+        $category->update($request->all());
+
+        return $category;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return $category;
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,12 +1,21 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    
+    protected $table = 'categories';
+
+    protected $primaryKey = 'id_category';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
     //Relacion de uno a muchos
     public function posts(){
         return $this->hasMany(Post::class);
